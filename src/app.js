43e3ba3ecdf758805bf73194f9d3c3baa424fe0d
lubnaabad <lubna_mosa@hotmail.com>
2018-10-03 12:25:19 +0300

    fix setup basic server relates #21

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 
@@ -10,17 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 app.disable('x-powered-by');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.engine(
-  'hbs',
-  handlebars({
-    extname: 'hbs',
-    layoutsDir: path.join(__dirname, 'views', 'layouts'),
-    partialsDir: path.join(__dirname, 'views', 'partials'),
-    defaultLayout: 'admin',
-  }),
-);
+
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
