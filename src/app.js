486aa38078a77ac11b6d99c500b6243d5a2b4f79
lubnaabad <lubna_mosa@hotmail.com>
2018-10-03 13:04:04 +0300

    fix setup basic server relates #21

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const compression = require('compression');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
 app.disable('x-powered-by');
 
 app.set('port', process.env.PORT || 3000);
