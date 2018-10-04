00a69c2301e4d9fbced42bbce08fa4e93fb5a53a
ali <ali.7@outlook.com>
2018-10-04 15:20:20 +0300

    add-books-sections-tabs-routes relate to #26'

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,7 +1,44 @@
 exports.getLibraryBooks = (request, response) => {
   response.render('view_books',
     {
-      viewBooks: 'active',
+      libraryBooks: 'active',
+      book: 'active',
+      layout: 'admin',
+      title: 'الكتب',
+      style: 'book',
+      js: 'book',
+    });
+};
+
+exports.getStoreBooks = (request, response) => {
+  response.render('view_books',
+    {
+      storeBooks: 'active',
+      book: 'active',
+      layout: 'admin',
+      title: 'الكتب',
+      style: 'book',
+      js: 'book',
+    });
+};
+
+exports.getBorrowedBooks = (request, response) => {
+  response.render('view_books',
+    {
+      borrowedBooks: 'active',
+      book: 'active',
+      layout: 'admin',
+      title: 'الكتب',
+      style: 'book',
+      js: 'book',
+    });
+};
+
+exports.getAddBookTab = (request, response) => {
+  response.render('view_books',
+    {
+      addBookTab: 'active',
+      book: 'active',
       layout: 'admin',
       title: 'الكتب',
       style: 'book',
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const adminHomePage = require('./admin_homePage.js');
 const user = require('./user.js');
 const login = require('./login');
 const books = require('./books');
+const adminHomePage = require('./admin_homePage.js');
 
 const router = express.Router();
 
@@ -11,12 +11,15 @@ router.get('/admin/', adminHomePage.get);
 router.get('/admin/users', user.getViewUser);
 router.get('/admin/borrowedUsers', user.getBorrowedUser);
 router.get('/admin/books/library', books.getLibraryBooks);
+router.get('/admin/books/store', books.getStoreBooks);
+router.get('/admin/books/borrowedBooks', books.getBorrowedBooks);
+router.get('/admin/books/addBook', books.getAddBookTab);
 
 module.exports = router;
 
 /* 
 /admin/books/library
 /admin/books/store
-/admin/books/addBook
 /admin/books/borrowedBooks
+/admin/books/addBook
 */
