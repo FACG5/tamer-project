05c8486b6e3958ae35087e10ee0eb58e0a083c5a
ali <ali.7@outlook.com>
2018-10-04 04:12:39 +0300

    add-the-rest-of-routes-to-controlles relates to #26

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const user = require('./user');
+const books = require('./books');
+const borrow = require('./borrow');
+const borrowedUsers = require('./borrowed_users');
 const adminHomePage = require('./admin_homePage.js');
 
 const router = express.Router();
 
 router.get('/admin/', adminHomePage.get);
+router.get('/admin/users', user.get);
+router.get('/admin/borrowedUsers', borrowedUsers.get);
+router.get('/admin/books', books.get);
+router.get('/admin/borrow', borrow.get);
 
 module.exports = router;
