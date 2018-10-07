c532098367ce59cb74b76fa06661971ccd50f201
lubnaabad <lubna_mosa@hotmail.com>
2018-10-07 11:00:08 +0300

    add queries getCategery, setbook and setcategery relates #16

---
+++ src/database/queries/set_book.js
@@ -0,0 +1,20 @@
+const dbConnection = require('../db_connection');
+
+const setBook = bookData => new Promise((resolve, reject) => {
+  const {
+    nameBookVal, nameAuthorVal, imageBookVal, descriptionVal, categorySerial,
+  } = bookData;
+  const sql = {
+    text: 'INSERT INTO book(name_book, name_author, image_url, description, category_serial) VALUES($1, $2, $3, $4, $5) RETURNING id,category_serial;    ',
+    values: [nameBookVal, nameAuthorVal, imageBookVal, descriptionVal, categorySerial],
+  };
+  dbConnection.query(sql, (err, res) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(res.rows);
+    }
+  });
+});
+
+module.exports = { setBook };
