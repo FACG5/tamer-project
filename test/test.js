2c07ca1bb47a4cbc5680557b80b5f391c86a03b8
lubnaabad <lubna_mosa@hotmail.com>
2018-10-07 11:01:17 +0300

    add test file relates #86

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 const test = require('tape');
 const supertest = require('supertest');
 const app = require('../src/app');
+const runDbBuild = require('../src/database/db_build');
+const { setBook } = require('../src/database/queries/set_book');
+const { setCategory } = require('../src/database/queries/set_category');
+const { getCategory } = require('../src/database/queries/get_category');
 
-// Admin login
 test('Home route with get method returns a status code of 200 ', (t) => {
   supertest(app)
     .get('/admin/login')
@@ -29,3 +32,66 @@ test('test for home page route ', (t) => {
       t.end();
     });
 });
+
+
+test('test for book page route ', (t) => {
+  supertest(app)
+    .get('/admin/books/add/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      if (err) {
+        t.error(err);
+      }
+      t.equal(res.text.includes('<title>الكتب</title>'), true, 'the page should have title \'الكتب\'');
+      t.end();
+    });
+});
+
+test('Test for the getCategory function', (t) => {
+  runDbBuild('db_bulid.sql', (err, res) => {
+    t.notOk(err);
+    return runDbBuild('fake_data.sql', () => {
+      getCategory()
+        .then((response) => {
+          t.equal(response.length > 0, true, 'getCategory returns data successfully ');
+          t.end();
+        })
+        .catch(error => t.error(error));
+    });
+  });
+});
+
+test('Test for the setBook function', (t) => {
+  runDbBuild('db_bulid.sql', (err, res) => {
+    t.notOk(err);
+    return runDbBuild('fake_data.sql', () => {
+      const data = {
+        nameBookVal: 'يوسف يامريم', nameAuthorVal: 'يامى أحمد', imageBookVal: '', descriptionVal: 'يوسف يامريم', categorySerial: '503',
+      };
+      setBook(data)
+        .then((response) => {
+          t.equal(typeof response, 'object', 'setBook returns data successfully ');
+        })
+        .catch(error => t.error(error));
+      t.end();
+    });
+  });
+});
+
+test('Test for the setCategory function', (t) => {
+  runDbBuild('db_bulid.sql', (err, res) => {
+    t.notOk(err);
+    return runDbBuild('fake_data.sql', () => {
+      const data = { nameCategoryVal: 'تاريخ', serialNumberVal: '507' };
+      setCategory(data)
+        .then((response) => {
+          t.equal(typeof response, 'object', 'setCategory returns data successfully ');
+        })
+        .catch(error => t.error(error));
+      t.end();
+    });
+  });
+});
+
+test.onFinish(() => { process.exit(0); });
