0766bbc02eebaf450ddef8594cdc144e13260646
ali <ali.7@outlook.com>
2018-10-07 08:29:43 +0300

    add-routes-to-post-login-and-logout

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,16 +3,21 @@ const user = require('./user.js');
 const login = require('./login');
 const books = require('./books');
 const adminHomePage = require('./admin_homePage.js');
+const logout = require('./logout');
+const { isLoggedIn } = require('./isLoggedIn');
 
 const router = express.Router();
 
 router.get('/admin/login', login.get);
-router.get('/admin/', adminHomePage.get);
-router.get('/admin/users', user.getViewUser);
-router.get('/admin/users/borrower', user.getBorrowedUser);
-router.get('/admin/books/library', books.getLibraryBooks);
-router.get('/admin/books/store', books.getStoreBooks);
-router.get('/admin/books/borrowed', books.getBorrowedBooks);
-router.get('/admin/books/add', books.getAddBookTab);
+router.post('/admin/login', login.post);
+router.get('/admin/logout', isLoggedIn, logout.get);
+
+router.get('/admin/', isLoggedIn, adminHomePage.get);
+router.get('/admin/users', isLoggedIn, user.getViewUser);
+router.get('/admin/users/borrower', isLoggedIn, user.getBorrowedUser);
+router.get('/admin/books/library', isLoggedIn, books.getLibraryBooks);
+router.get('/admin/books/store', isLoggedIn, books.getStoreBooks);
+router.get('/admin/books/borrowed', isLoggedIn, books.getBorrowedBooks);
+router.get('/admin/books/add', isLoggedIn, books.getAddBookTab);
 
 module.exports = router;
