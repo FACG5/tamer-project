82163d57b4720ccc24e5254f599926b5b2e633cd
ali <ali.7@outlook.com>
2018-10-08 12:00:08 +0300

    add-a-middleware-to-the-secure-routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,12 +5,13 @@ const books = require('./books');
 const borrow = require('./borrow');
 const adminHomePage = require('./admin_homePage.js');
 const logout = require('./logout');
-const { isLoggedIn } = require('./isLoggedIn');
+const { isLoggedIn } = require('./is_logged_in.js');
 const websiteHomePage = require('./website_homepage');
 const error = require('./error');
 
 const router = express.Router();
 
+// unsecure routes
 // website homepage route
 router.get('/', websiteHomePage.get);
 
@@ -18,18 +19,21 @@ router.get('/', websiteHomePage.get);
 router.get('/admin/login', login.get);
 router.post('/admin/login', login.post);
 
+// secure routes
+router.use(isLoggedIn);
+
 // admin control panel routes
-router.get('/admin/logout', isLoggedIn, logout.get);
-router.get('/admin/', isLoggedIn, adminHomePage.get);
-router.get('/admin/users', isLoggedIn, user.getViewUser);
-router.get('/admin/users/borrower', isLoggedIn, user.getBorrowedUser);
-router.get('/admin/books/library', isLoggedIn, books.getLibraryBooks);
-router.get('/admin/books/store', isLoggedIn, books.getStoreBooks);
-router.get('/admin/books/borrowed', isLoggedIn, books.getBorrowedBooks);
-router.get('/admin/books/add', isLoggedIn, books.getAddBookTab);
+router.get('/admin/logout', logout.get);
+router.get('/admin/', adminHomePage.get);
+router.get('/admin/users', user.getViewUser);
+router.get('/admin/users/borrower', user.getBorrowedUser);
+router.get('/admin/books/library', books.getLibraryBooks);
+router.get('/admin/books/store', books.getStoreBooks);
+router.get('/admin/books/borrowed', books.getBorrowedBooks);
+router.get('/admin/books/add', books.getAddBookTab);
 router.post('/admin/books/category', books.addCategory);
 router.post('/admin/books/', books.addBook);
-router.get('/admin/borrow', isLoggedIn, borrow.get);
+router.get('/admin/borrow', borrow.get);
 
 router.use(error.client);
 router.use(error.server);
