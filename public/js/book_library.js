3877095fafdcf2321263ba15c7cb4db2774dfe4b
lubnaabad <lubna_mosa@hotmail.com>
2018-10-08 11:52:06 +0300

    front-end addLibrart page relates #61

---
+++ public/js/book_library.js
@@ -0,0 +1,53 @@
+const numberCopies = document.getElementById('numberCopies');
+const numberCopiesError = document.getElementById('numberCopiesError');
+const bookshelf = document.getElementById('bookshelf');
+const bookshelfError = document.getElementById('bookshelfError');
+const section = document.getElementById('section');
+const sectionError = document.getElementById('sectionError');
+const addLibraryBook = document.getElementById('addLibraryBook');
+
+numberCopies.addEventListener('focusout', (e) => {
+  check(numberCopies, numberCopiesError, 'هذا الحقل مطلوب');
+});
+
+bookshelf.addEventListener('focusout', (e) => {
+  check(bookshelf, bookshelfError, 'هذا الحقل مطلوب');
+});
+
+section.addEventListener('focusout', (e) => {
+  check(section, sectionError, 'هذا الحقل مطلوب');
+});
+
+
+addLibraryBook.addEventListener('click', (e) => {
+  const checkNumberCopies = check(numberCopies, numberCopiesError, 'هذا الحقل مطلوب');
+  const checkBookshelf = check(bookshelf, bookshelfError, 'هذا الحقل مطلوب');
+  const checkSection = check(section, sectionError, 'هذا الحقل مطلوب');
+  if (checkNumberCopies && checkBookshelf && checkSection) {
+    for (let copyId = 1; copyId <= numberCopies.value; copyId += 1) {
+      const copyIdVal = copyId;
+      const bookshelfVal = bookshelf.value;
+      const sectionVal = section.value;
+      const bookId = next.value;
+      const data = {
+        bookId,
+        bookshelfVal,
+        sectionVal,
+        copyIdVal,
+      };
+      fetch(`/admin/books/${bookId}/library`, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json; charset=utf-8' },
+        body: JSON.stringify(data),
+      })
+        .then(result => result.json())
+        .then((response) => {
+          swal('Good job!', response.message, 'success').then(() => {
+            JSON.stringify(response);
+          });
+        })
+        .catch(error => swal('Error while adding category !', error, 'error'));
+    }
+  }
+});
