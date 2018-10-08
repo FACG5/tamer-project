9e943f0328a79f8e84cb9922665350090f0a9bd3
asmaa-ezz <you@example.com>
2018-10-09 00:46:55 +0300

    Add queries getUsers to get all data in all user

---
+++ src/database/queries/test/view_user.js
@@ -0,0 +1,13 @@
+const dbConnection = require('../../db_connection');
+
+const getUsers = () => new Promise((resolve, reject) => {
+  const sql = 'SELECT name AS "nameUser", address, mobile_number As "mobileNumber" FROM "user";';
+  dbConnection.query(sql, (error, res) => {
+    if (error) return reject(error);
+    return resolve(res.rows);
+  });
+});
+
+module.exports = {
+  getUsers,
+};
