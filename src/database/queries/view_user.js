3e709de23b22b1a5eaaa6ef1bffaa279d3167063
asmaa-ezz <you@example.com>
2018-10-09 16:12:47 +0300

    Edit in qureies to add return id borrow

--- src/database/queries/view_user.js
+++ src/database/queries/view_user.js
@@ -9,7 +9,7 @@ const getUsers = () => new Promise((resolve, reject) => {
 });
 
 const getBorrower = () => new Promise((resolve, reject) => {
-  const sql = 'SELECT  "user".name AS "nameUser" ,"user".address,"user".mobile_number As "mobileNumber",borrow.start_date AS "startDate" ,borrow.end_date AS "endDate" ,book.name_book As nameBook FROM  "user"  join borrow ON borrow.user_id = "user".id join library ON library.id = borrow.book_library_id join book ON book.id = library.book_id';
+  const sql = 'SELECT borrow.id AS "idBorrow", "user".name AS "nameUser" ,"user".address,"user".mobile_number As "mobileNumber",TO_CHAR(borrow.start_date, \'YYYY-MM-DD\') AS "startDate", TO_CHAR(borrow.end_date, \'YYYY-MM-DD\') AS "endDate" ,book.name_book As nameBook FROM  "user"  join borrow ON borrow.user_id = "user".id join library ON library.id = borrow.book_library_id join book ON book.id = library.book_id';
   dbConnection.query(sql, (error, res) => {
     if (error) return reject(error);
     return resolve(res.rows);
