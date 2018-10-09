801c2b18c4ff4618daf657cc7ebee107f2280577
asmaa-ezz <you@example.com>
2018-10-09 15:47:07 +0300

    Add queres getBorrower in database

--- src/database/queries/view_user.js
+++ src/database/queries/view_user.js
@@ -8,6 +8,15 @@ const getUsers = () => new Promise((resolve, reject) => {
   });
 });
 
+const getBorrower = () => new Promise((resolve, reject) => {
+  const sql = 'SELECT  "user".name AS "nameUser" ,"user".address,"user".mobile_number As "mobileNumber",borrow.start_date AS "startDate" ,borrow.end_date AS "endDate" ,book.name_book As nameBook FROM  "user"  join borrow ON borrow.user_id = "user".id join library ON library.id = borrow.book_library_id join book ON book.id = library.book_id';
+  dbConnection.query(sql, (error, res) => {
+    if (error) return reject(error);
+    return resolve(res.rows);
+  });
+});
+
 module.exports = {
   getUsers,
+  getBorrower,
 };
