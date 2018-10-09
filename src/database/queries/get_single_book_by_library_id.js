3b599d3d8a4e0b87f5497677aab1588e9cdc60f8
ali <ali.7@outlook.com>
2018-10-09 15:53:13 +0300

    add-getSingleBookByLibraryId-query relates #16

--- src/database/queries/get_single_book_by_library_id.js
+++ src/database/queries/get_single_book_by_library_id.js
@@ -3,7 +3,7 @@ const dbConnection = require('../db_connection');
 const getSingleBookByLibraryId = data => new Promise((resolve, reject) => {
   const { libraryId } = data;
   const sql = {
-    text: 'SELECT book.name_book AS "nameBook", book.name_author AS "nameAuthor", book.description, book.category_serial AS "category", library.bookshelf AS "bookShelf", library.section, library.copy_id AS "copyId", book.image_url AS "img", library.id AS "libraryId" FROM library JOIN book on library.book_id = book.id WHERE library.id  = $1 ',
+    text: 'SELECT book.name_book AS "nameBook", book.name_author AS "nameAuthor", book.description, book.category_serial AS "categorySerial", library.bookshelf AS "bookShelf", library.section, library.copy_id AS "copyId", book.image_url AS "img", library.id AS "libraryId", category.name AS "categoryName" FROM library JOIN book on library.book_id = book.id JOIN category on category.category_serial = book.category_serial WHERE library.id  = $1 ',
     values: [libraryId],
   };
 
