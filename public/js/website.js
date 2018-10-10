8ae43b9fc402e1216dc8eca139b9ecbf07e65344
asmaa-ezz <you@example.com>
2018-10-10 11:39:32 +0300

    Edit frount-end with website Relates #82

--- public/js/website.js
+++ public/js/website.js
@@ -1,5 +1,34 @@
 const search = document.getElementById('search');
+const inputSearch = document.getElementById('input-search');
+const submitSearch = document.getElementById('submit-search');
 
-const searchBook = () => {
-  search.classList.add('main__search--book-visible');
-};
+submitSearch.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (inputSearch.value.trim() !== '') {
+    const object = {
+      string: inputSearch.value,
+    };
+    fetch('/', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      credentials: 'same-origin',
+      body: JSON.stringify(object),
+    })
+      .then(result => result.json())
+      .then((result) => {
+        console.log(result);
+        search.classList.add('main__search--book-visible');
+        const div = document.createElement('div');
+        const h5 = document.createElement('h5');
+        h5.textContent = 'كتاب';
+        div.appendChild(h5);
+        const img = document.createElement('img');
+        img.src = 'https://edition-medali.tn/img/p/ar-default.jpg';
+        div.appendChild(img);
+        search.appendChild(div);
+      })
+      .catch(err => swal(err, ' ', 'error'));
+  } else {
+    swal('Error !', 'أرجوا أن تدخل النص', 'error');
+  }
+});
