de373a92d866e1c2f58111ed9fc97d3b5310a1a8
ali <ali.7@outlook.com>
2018-10-10 15:32:15 +0300

    recover-deleted-tests-after-solving-conflect

--- src/controllers/test/test.js
+++ src/controllers/test/test.js
@@ -476,4 +476,29 @@ test('test for delete library book - with cookie and auth ', (t) => {
     });
 });
 
+test('test for add borrowpage page route  - with cookie and auth ', (t) => {
+  supertest(app)
+    .post('/admin/borrow/')
+    .set('Cookie', ['data = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6ImFkbWluIiwiaWF0IjoxNTM4OTExNzQxfQ.gQe7y4oF7wlL4oPAXdzMmNTwGlE2d69FyehJcOyiYLg'])
+    .send({
+      mobileNumberVal: '0599112233',
+    })
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      if (err) {
+        t.error(err);
+      }
+      const response = JSON.parse(res.text);
+      t.equal(response.resultUser[0].userId, 1, 'id should return 1');
+      t.equal(response.resultUser[0].name, 'أسماء', 'name should return أسماء');
+      t.equal(response.resultUser[0].address, 'غزة - النصر', 'address returns \' غزة - النصر\' ');
+      t.equal(response.resultBorrowedBooksByUserId[0].nameBook, 'ليلى الحمقاء', 'name returns \'ليلى الحمقاء\' ');
+      t.equal(response.resultBorrowedBooksByUserId[0].endDate, '2018-09-25', 'endDate returns \'2018-09-25\' ');
+      t.equal(response.resultBorrowedBooksByUserId[0].serialNumber, '501.1.5.2', 'serialNumber returns \'501.1.5.2\' ');
+      t.equal(response.resultBorrowedBooksByUserId[0].idBorrow, 1, 'section returns idBorrow ');
+      t.end();
+    });
+});
+
 test.onFinish(() => { process.exit(0); });
