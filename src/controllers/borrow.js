3aaff28fba44e5f11db7f9a3a8d49cbee06f0123
lubnaabad <lubna_mosa@hotmail.com>
2018-10-10 09:42:02 +0300

    create-function getUser and getBorrowedBooksByUserId in the borrow.js relates #89

--- src/controllers/borrow.js
+++ src/controllers/borrow.js
@@ -1,3 +1,6 @@
+const { getBorrowedBooksByUserId } = require('../database/queries/get_borrowedBooksByUserId');
+const { getUser } = require('../database/queries/get_user');
+
 exports.get = (request, response) => {
   response.render('view_borrow',
     {
@@ -7,5 +10,34 @@ exports.get = (request, response) => {
       title: 'اﻹعارة',
       style: 'borrow',
       js: ['borrow'],
+    });
+};
+
+exports.post = (request, response, next) => {
+  getUser(request.body)
+    .then((resultUser) => {
+      const resultBorrowedBooksByUserId = [];
+      if (resultUser.length) {
+        const { userId } = resultUser[0];
+        getBorrowedBooksByUserId(userId)
+          .then((resarr) => {
+            resarr.forEach((res) => {
+              const serialNumber = `${res.category}.${res.bookshelf}.${res.section}.${res.copyId}`;
+              const data = {
+                nameBook: res.nameBook,
+                endDate: res.endDate,
+                serialNumber,
+                idBorrow: res.idBorrow,
+              };
+              resultBorrowedBooksByUserId.push(data);
+            });
+            response.send(JSON.stringify({ resultUser, resultBorrowedBooksByUserId }));
+          });
+      } else {
+        response.send(JSON.stringify({ resultUser, resultBorrowedBooksByUserId }));
+      }
+    })
+    .catch((err) => {
+      next(err);
     });
 };
