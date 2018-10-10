b5d73c52a6e6c99ee91086a373bfcf21bc9cdd03
asmaa-ezz <you@example.com>
2018-10-10 14:12:17 +0300

    Edit in style website

--- src/database/queries/website.js
+++ src/database/queries/website.js
@@ -1,7 +1,6 @@
 const dbConnection = require('../db_connection');
 
-const getSearchedBook = str => new Promise((resolve, reject) => {
-  console.log(str);
+const getSearchedBook = str => new Promise((resolve, reject) => {;
   const sql = {
     text: 'SELECT library.id AS "idLibrary", book.name_book AS "nameBook", book.name_author AS "nameAuthor", book.category_serial AS "category", library.bookshelf AS "bookShelf", library.section, library.copy_id AS "copyId", COALESCE(borrow.id, 0) AS "caseBook"FROM library JOIN book on library.book_id = book.id LEFT JOIN borrow on book_library_id= library.id where book.name_book ILIKE $1 OR book.name_author ILIKE $2 ORDER BY library.id',
     values: [`%${str}%`, `%${str}%`],
