d00ada77b34b0dbe9da14509ecea2c364fab9f44
ali <ali.7@outlook.com>
2018-10-11 19:19:53 +0300

    add-editLibraryinfo-query-that-return-library-info-to-edit relates #10

--- src/database/queries/edit_book.js
+++ src/database/queries/edit_book.js
@@ -17,4 +17,19 @@ const editBookInfo = bookData => new Promise((resolve, reject) => {
   });
 });
 
-module.exports = { editBookInfo };
+const editLibraryInfo = libraryData => new Promise((resolve, reject) => {
+  const { bookShelfVal, sectionVal, libraryId } = libraryData;
+  const sql = {
+    text: 'UPDATE library SET bookshelf=$1, section=$2 where id =$3',
+    values: [bookShelfVal, sectionVal, libraryId],
+  };
+  dbconnection.query(sql, (err, res) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(res.rows);
+    }
+  });
+});
+
+module.exports = { editBookInfo, editLibraryInfo };
