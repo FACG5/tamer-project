60c2d8e1de591e7f9cee5cc6679b480a65afe5c6
ali <ali.7@outlook.com>
2018-10-12 13:49:22 +0300

    modify-getSingleBookByStoreId-query-to-return-bookID relates #56

--- src/database/queries/get_single_book_by_store_id.js
+++ src/database/queries/get_single_book_by_store_id.js
@@ -3,7 +3,7 @@ const dbConnection = require('../db_connection');
 const getSingleBookByStoreId = data => new Promise((resolve, reject) => {
   const { storeId } = data;
   const sql = {
-    text: 'SELECT book.name_book AS "nameBook", book.name_author AS "nameAuthor", book.description, book.category_serial AS "categorySerial", book.image_url AS "img", store.copy_number AS "copyNumber", store.id AS "storeId", category.name AS "categoryName" FROM store JOIN book on store.book_id = book.id JOIN category on category.category_serial = book.category_serial WHERE store.id  = $1 ',
+    text: 'SELECT book.id AS "bookId", book.name_book AS "nameBook", book.name_author AS "nameAuthor", book.description, book.category_serial AS "categorySerial", book.image_url AS "img", store.copy_number AS "copyNumber", store.id AS "storeId", category.name AS "categoryName" FROM store JOIN book on store.book_id = book.id JOIN category on category.category_serial = book.category_serial WHERE store.id  = $1 ',
     values: [storeId],
   };
 
