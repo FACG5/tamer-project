e61d90b4a8e1bee226a186b2a75e6957f9dcca21
lubnaabad <lubna_mosa@hotmail.com>
2018-12-31 20:14:39 +0200

    get libraryIdVal from data relates #154

--- src/database/queries/get_library_id.js
+++ src/database/queries/get_library_id.js
@@ -1,10 +1,12 @@
 const dbConnection = require('../db_connection');
 
 const getLibraryId = data => new Promise((resolve, reject) => {
-  const { bookshelfVal, copyIdVal, sectionsVal } = data;
+  const {
+    bookshelfVal, copyIdVal, sectionsVal, libraryIdVal,
+  } = data;
   const sql = {
-    text: 'select id AS "LibraryId" from library where bookshelf=$1 and copy_id=$2 and section=$3 ',
-    values: [bookshelfVal, copyIdVal, sectionsVal],
+    text: 'select id AS "LibraryId" from library where bookshelf=$1 and copy_id=$2 and section=$3  and id=$4 ',
+    values: [bookshelfVal, copyIdVal, sectionsVal, libraryIdVal],
   };
 
   dbConnection.query(sql, (err, res) => {
