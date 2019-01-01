085bce1677bf7f63cf1a4040541ae81aa0cdf5d8
ali <ali.7@outlook.com>
2019-01-01 20:46:04 +0200

    add-editSingleCategoryView-function-to-categories relate #151

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,4 +1,5 @@
 const { getCategory } = require('../database/queries/get_category');
+const { getSingleCategory } = require('../database/queries/get_single_category');
 
 exports.getCategoryTab = (request, response, next) => {
   getCategory()
@@ -14,6 +15,25 @@ exports.getCategoryTab = (request, response, next) => {
           resCategory,
         });
     }).catch((err) => {
+      next(err);
+    });
+};
+
+exports.editSingleCategoryView = (request, response, next) => {
+  const id = request.params;
+  getSingleCategory(id)
+    .then((results) => {
+      response.render('edit_category',
+        {
+          book: 'active',
+          layout: 'admin',
+          title: 'تعديل التصنيف',
+          style: ['book', 'category'],
+          js: ['edit_category'],
+          results,
+        });
+    })
+    .catch((err) => {
       next(err);
     });
 };
